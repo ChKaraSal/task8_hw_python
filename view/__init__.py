4380def35a75ae88c14fa2d0d28d43f8f1429ff6
chayan <chayan.karasal@internet.ru>
2023-06-21 08:18:11 +0700

    task8

---
+++ view/__init__.py
@@ -0,0 +1,4 @@
+from .console import menu, show_contacts, print_message, input_contact, input_return, add_data
+from .text import *
+
+__all__ = ['menu', 'show_contacts', 'input_contact', 'input_return', 'saved_data']
